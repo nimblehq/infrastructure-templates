82ef984c8e1fdfd21bdde95d7f3d39d401b3e2a5
sleepylee <cs.leetrung@gmail.com>
2018-09-06 16:08:51 +0700

    Update variables

--- heroku/variables.tf
+++ heroku/variables.tf
@@ -1,19 +1,41 @@
-variable "email" {
+variable "heroku_account_email" {
   description = "Heroku Access Email"
   default = "dev@nimbl3.com"
 }
+
+variable "heroku_token" { }
 
 variable "app_name" {
   description = "Application name"
   default = "nimbl3-terraform-test-app"
 }
 
-variable "heroku_token" {
-  description = "Heroku API Access Token"
-  default = "${env.HEROKU_TOKEN}"
-}
-
 variable "region" {
   description = "Heroku hosting region"
   default = "us"
+}
+
+variable "postgres_plan" {
+  description = "Heroku postgres db add-on plan (FREE)"
+  default = "heroku-postgresql:hobby-dev"
+}
+
+variable "newrelic_plan" {
+  description = "Heroku Newrelic add-on plan (FREE)"
+  default = "newrelic:wayne"
+}
+
+variable "papertrail_plan" {
+  description = "Heroku Papertrail add-on plan (FREE)"
+  default = "papertrail:choklad"
+}
+
+variable "sentry_plan" {
+  description = "Heroku Sentry add-on plan (FREE)"
+  default = "sentry:f1"
+}
+
+variable "redis_plan" {
+  description = "Heroku Redis add-on plan (FREE)"
+  default = "heroku-redis:hobby-dev"
 }
