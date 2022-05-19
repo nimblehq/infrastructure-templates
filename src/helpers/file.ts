c2fd0cce18dbaa55308b728b83742f737a54999e
Hoang Mirs <hoang.mirs@gmail.com>
2022-05-19 17:30:04 +0700

    Revert baseUrl

--- src/helpers/file.ts
+++ src/helpers/file.ts
@@ -1,16 +1,16 @@
 import * as fs from 'fs';
 import path = require('path');
 
-const CURRENT_DIR = path.join(__dirname, '..', 'skeleton');
+const TEMPLATE_DIR = path.join(__dirname, '..', '..', 'skeleton');
 
 const copyFile = (source: string, target: string): void => {
-  const sourcePath = path.join(CURRENT_DIR, source);
+  const sourcePath = path.join(TEMPLATE_DIR, source);
 
   fs.copyFileSync(sourcePath, target);
 };
 
 const copyDir = (source: string, target: string): void => {
-  const sourcePath = path.join(CURRENT_DIR, source);
+  const sourcePath = path.join(TEMPLATE_DIR, source);
   const targetExists = fs.existsSync(target);
   if (!targetExists) {
     fs.mkdirSync(target);
@@ -20,7 +20,7 @@ const copyDir = (source: string, target: string): void => {
   files.forEach((file) => {
     const sourceFile = path.join(source, file);
     const targetFile = path.join(target, file);
-    if (fs.lstatSync(path.join(CURRENT_DIR, sourceFile)).isDirectory()) {
+    if (fs.lstatSync(path.join(TEMPLATE_DIR, sourceFile)).isDirectory()) {
       copyDir(sourceFile, targetFile);
     } else {
       copyFile(sourceFile, targetFile);
