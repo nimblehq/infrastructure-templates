017a68cb52e682e1ef4a801f4beffb806c9e425d
Hoang Mirs <hoang.mirs@gmail.com>
2022-05-19 17:50:09 +0700

    Fix path

--- src/helpers/file.ts
+++ src/helpers/file.ts
@@ -1,16 +1,19 @@
 import * as fs from 'fs';
 import path = require('path');
 
-const TEMPLATE_DIR = path.join(__dirname, '..', '..', 'skeleton');
+const ROOT_DIR = path.join(__dirname, '..', '..');
+const TEMPLATE_DIR =
+  process.env.NODE_ENV === 'development' ? 'skeleton' : 'dist/skeleton';
+const TEMPLATE_PATH = path.join(ROOT_DIR, TEMPLATE_DIR);
 
 const copyFile = (source: string, target: string): void => {
-  const sourcePath = path.join(TEMPLATE_DIR, source);
+  const sourcePath = path.join(TEMPLATE_PATH, source);
 
   fs.copyFileSync(sourcePath, target);
 };
 
 const copyDir = (source: string, target: string): void => {
-  const sourcePath = path.join(TEMPLATE_DIR, source);
+  const sourcePath = path.join(TEMPLATE_PATH, source);
   const targetExists = fs.existsSync(target);
   if (!targetExists) {
     fs.mkdirSync(target);
@@ -20,7 +23,7 @@ const copyDir = (source: string, target: string): void => {
   files.forEach((file) => {
     const sourceFile = path.join(source, file);
     const targetFile = path.join(target, file);
-    if (fs.lstatSync(path.join(TEMPLATE_DIR, sourceFile)).isDirectory()) {
+    if (fs.lstatSync(path.join(TEMPLATE_PATH, sourceFile)).isDirectory()) {
       copyDir(sourceFile, targetFile);
     } else {
       copyFile(sourceFile, targetFile);
