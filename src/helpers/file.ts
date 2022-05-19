7150637d89304c0e7b63008bc89b7fee7e12c015
Hoang Mirs <hoang.mirs@gmail.com>
2022-05-19 23:00:03 +0700

    Add more helpers

--- src/helpers/file.ts
+++ src/helpers/file.ts
@@ -6,6 +6,10 @@ const TEMPLATE_DIR =
   process.env.NODE_ENV === 'development' ? 'skeleton' : 'dist/skeleton';
 const TEMPLATE_PATH = path.join(ROOT_DIR, TEMPLATE_DIR);
 
+const appendToFile = (target: string, content: string): void => {
+  fs.appendFileSync(target, content);
+};
+
 const copyFile = (source: string, target: string): void => {
   const sourcePath = path.join(TEMPLATE_PATH, source);
 
@@ -16,7 +20,7 @@ const copyDir = (source: string, target: string): void => {
   const sourcePath = path.join(TEMPLATE_PATH, source);
   const targetExists = fs.existsSync(target);
   if (!targetExists) {
-    fs.mkdirSync(target);
+    fs.mkdirSync(target, { recursive: true });
   }
 
   const files = fs.readdirSync(sourcePath);
@@ -31,4 +35,58 @@ const copyDir = (source: string, target: string): void => {
   });
 };
 
-export { copyFile, copyDir };
+const createFile = (target: string, content: string): void => {
+  const targetExists = fs.existsSync(target);
+  if (!targetExists) {
+    fs.writeFileSync(target, content);
+  }
+};
+
+const injectToFile = (
+  target: string,
+  content: string,
+  {
+    insert_before = '',
+    insert_after = '',
+  }: { insert_before?: string; insert_after?: string } = {}
+): void => {
+  fs.readFile(target, (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    const lines = data.toString().split('\n');
+
+    if (insert_before) {
+      const index = lines.findIndex((line) => line.includes(insert_before));
+      if (index !== -1) {
+        lines.splice(index, 0, content);
+      }
+    }
+
+    if (insert_after) {
+      const index = lines.findIndex((line) => line.includes(insert_after));
+      if (index !== -1) {
+        lines.splice(index + 1, 0, content);
+      }
+    }
+
+    const newContent = lines.join('\n');
+    fs.writeFileSync(target, newContent);
+  });
+};
+
+const renameFile = (source: string, target: string): void => {
+  const sourcePath = path.join(TEMPLATE_PATH, source);
+  const targetPath = path.join(TEMPLATE_PATH, target);
+  fs.renameSync(sourcePath, targetPath);
+};
+
+export {
+  appendToFile,
+  copyDir,
+  copyFile,
+  createFile,
+  injectToFile,
+  renameFile,
+};
