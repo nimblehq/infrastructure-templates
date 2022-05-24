5ba2990faa77a5792fe54bf58d23ba62050fa410
Hoang Mirs <hoang.mirs@gmail.com>
2022-05-24 11:18:49 +0700

    Run lint:fix

--- src/helpers/file.ts
+++ src/helpers/file.ts
@@ -1,148 +1,148 @@
-import * as fs from 'fs';
+import * as fs from 'node:fs'
 import path = require('path');
-import { GenerateOption } from '../commands/generate';
+import {GenerateOption} from '../commands/generate'
 
-const ROOT_DIR = path.join(__dirname, '..', '..');
+const ROOT_DIR = path.join(__dirname, '..', '..')
 const TEMPLATE_DIR =
-  process.env.NODE_ENV === 'development' ? 'skeleton' : 'dist/skeleton';
-const TEMPLATE_PATH = path.join(ROOT_DIR, TEMPLATE_DIR);
+  process.env.NODE_ENV === 'development' ? 'skeleton' : 'dist/skeleton'
+const TEMPLATE_PATH = path.join(ROOT_DIR, TEMPLATE_DIR)
 
 const getTargetPath = (file: string, options: GenerateOption): string => {
-  const { projectName } = options;
-  const targetPath = path.join(process.cwd(), projectName);
+  const {projectName} = options
+  const targetPath = path.join(process.cwd(), projectName)
 
-  return path.join(targetPath, file);
-};
+  return path.join(targetPath, file)
+}
 
 const appendToFile = (
   target: string,
   content: string,
-  options: GenerateOption
+  options: GenerateOption,
 ): void => {
-  const targetPath = getTargetPath(target, options);
+  const targetPath = getTargetPath(target, options)
 
-  fs.appendFileSync(targetPath, content);
-};
+  fs.appendFileSync(targetPath, content)
+}
 
 const copyFile = (
   source: string,
   target: string,
-  options: GenerateOption
+  options: GenerateOption,
 ): void => {
-  const sourcePath = path.join(TEMPLATE_PATH, source);
-  const targetPath = getTargetPath(target, options);
-  const targetDir = path.dirname(targetPath);
-  const targetExists = fs.existsSync(targetPath);
+  const sourcePath = path.join(TEMPLATE_PATH, source)
+  const targetPath = getTargetPath(target, options)
+  const targetDir = path.dirname(targetPath)
+  const targetExists = fs.existsSync(targetPath)
   if (!targetExists) {
-    fs.mkdirSync(targetDir, { recursive: true });
+    fs.mkdirSync(targetDir, {recursive: true})
   }
 
-  fs.copyFileSync(sourcePath, targetPath);
-};
+  fs.copyFileSync(sourcePath, targetPath)
+}
 
 const copyDir = (
   source: string,
   target: string,
-  options: GenerateOption
+  options: GenerateOption,
 ): void => {
-  const sourcePath = path.join(TEMPLATE_PATH, source);
-  const targetPath = getTargetPath(target, options);
-  const targetExists = fs.existsSync(targetPath);
+  const sourcePath = path.join(TEMPLATE_PATH, source)
+  const targetPath = getTargetPath(target, options)
+  const targetExists = fs.existsSync(targetPath)
   if (!targetExists) {
-    fs.mkdirSync(targetPath, { recursive: true });
+    fs.mkdirSync(targetPath, {recursive: true})
   }
 
-  const files = fs.readdirSync(sourcePath);
-  files.forEach((file) => {
-    const sourceFile = path.join(source, file);
-    const targetFile = path.join(target, file);
+  const files = fs.readdirSync(sourcePath)
+  for (const file of files) {
+    const sourceFile = path.join(source, file)
+    const targetFile = path.join(target, file)
     if (fs.lstatSync(path.join(TEMPLATE_PATH, sourceFile)).isDirectory()) {
-      copyDir(sourceFile, targetFile, options);
+      copyDir(sourceFile, targetFile, options)
     } else {
-      copyFile(sourceFile, targetFile, options);
+      copyFile(sourceFile, targetFile, options)
     }
-  });
-};
+  }
+}
 
 const createFile = (
   target: string,
   content: string,
-  options: GenerateOption
+  options: GenerateOption,
 ): void => {
-  const targetPath = getTargetPath(target, options);
-  const targetExists = fs.existsSync(targetPath);
+  const targetPath = getTargetPath(target, options)
+  const targetExists = fs.existsSync(targetPath)
 
   if (!targetExists) {
-    fs.writeFileSync(targetPath, content);
+    fs.writeFileSync(targetPath, content)
   }
-};
+}
 
 const deleteFile = (target: string, options: GenerateOption): void => {
-  const targetPath = getTargetPath(target, options);
-  const targetExists = fs.existsSync(targetPath);
+  const targetPath = getTargetPath(target, options)
+  const targetExists = fs.existsSync(targetPath)
 
   if (targetExists) {
-    fs.unlinkSync(targetPath);
+    fs.unlinkSync(targetPath)
   }
-};
+}
 
 const deleteDir = (target: string, options: GenerateOption): void => {
-  const targetPath = getTargetPath(target, options);
-  const targetExists = fs.existsSync(targetPath);
+  const targetPath = getTargetPath(target, options)
+  const targetExists = fs.existsSync(targetPath)
 
   if (targetExists) {
-    fs.rmdirSync(targetPath, { recursive: true });
+    fs.rmdirSync(targetPath, {recursive: true})
   }
-};
+}
 
 interface InjectToFileOptions {
   options?: GenerateOption;
-  insert_before?: string;
-  insert_after?: string;
+  insertBefore?: string;
+  insertAfter?: string;
 }
 
 const injectToFile = (
   target: string,
   content: string,
-  { insert_before = '', insert_after = '', options }: InjectToFileOptions = {}
+  {insertBefore = '', insertAfter = '', options}: InjectToFileOptions = {},
 ): void => {
-  const targetPath = options ? getTargetPath(target, options) : target;
+  const targetPath = options ? getTargetPath(target, options) : target
 
   fs.readFile(targetPath, (err, data) => {
     if (err) {
-      throw err;
+      throw err
     }
 
-    const lines = data.toString().split('\n');
+    const lines = data.toString().split('\n')
 
-    if (insert_before) {
-      const index = lines.findIndex((line) => line.includes(insert_before));
+    if (insertBefore) {
+      const index = lines.findIndex(line => line.includes(insertBefore))
       if (index !== -1) {
-        lines.splice(index, 0, content);
+        lines.splice(index, 0, content)
       }
     }
 
-    if (insert_after) {
-      const index = lines.findIndex((line) => line.includes(insert_after));
+    if (insertAfter) {
+      const index = lines.findIndex(line => line.includes(insertAfter))
       if (index !== -1) {
-        lines.splice(index + 1, 0, content);
+        lines.splice(index + 1, 0, content)
       }
     }
 
-    const newContent = lines.join('\n');
-    fs.writeFileSync(targetPath, newContent);
-  });
-};
+    const newContent = lines.join('\n')
+    fs.writeFileSync(targetPath, newContent)
+  })
+}
 
 const renameFile = (
   source: string,
   target: string,
-  options: GenerateOption
+  options: GenerateOption,
 ): void => {
-  const sourcePath = getTargetPath(source, options);
-  const targetPath = getTargetPath(target, options);
-  fs.renameSync(sourcePath, targetPath);
-};
+  const sourcePath = getTargetPath(source, options)
+  const targetPath = getTargetPath(target, options)
+  fs.renameSync(sourcePath, targetPath)
+}
 
 export {
   appendToFile,
@@ -153,4 +153,4 @@ export {
   createFile,
   injectToFile,
   renameFile,
-};
+}
--- src/templates/aws/advanced.ts
+++ src/templates/aws/advanced.ts
@@ -1,44 +1,43 @@
-import * as dedent from 'dedent';
-import { GenerateOption } from '../../commands/generate';
+import * as dedent from 'dedent'
+import {GenerateOption} from '../../commands/generate'
 import {
   appendToFile,
   copyDir,
   copyFile,
-  injectToFile,
-} from '../../helpers/file';
+} from '../../helpers/file'
 
 export default class Advanced {
   options: GenerateOption;
 
   constructor(options: GenerateOption) {
-    this.options = options;
+    this.options = options
   }
 
   static run(options: GenerateOption): void {
-    const advanced = new Advanced(options);
-    advanced.applyTemplate();
+    const advanced = new Advanced(options)
+    advanced.applyTemplate()
   }
 
   private applyTemplate(): void {
-    this.applyCommon();
-    this.applyVPC();
+    this.applyCommon()
+    this.applyVPC()
   }
 
   private applyCommon(): void {
-    copyFile('aws/main.tf', 'main.tf', this.options);
-    copyFile('aws/outputs.tf', 'outputs.tf', this.options);
-    copyFile('aws/variables.tf', 'variables.tf', this.options);
+    copyFile('aws/main.tf', 'main.tf', this.options)
+    copyFile('aws/outputs.tf', 'outputs.tf', this.options)
+    copyFile('aws/variables.tf', 'variables.tf', this.options)
   }
 
   private applyVPC(): void {
-    copyDir('aws/modules/vpc', 'modules/vpc', this.options);
+    copyDir('aws/modules/vpc', 'modules/vpc', this.options)
 
     const vpcOutputContent = dedent`
     output "vpc_id" {
       description = "VPC ID"
       value       = "module.vpc.vpc_id"
-    }`;
-    appendToFile('outputs.tf', vpcOutputContent, this.options);
+    }`
+    appendToFile('outputs.tf', vpcOutputContent, this.options)
 
     const vpcModuleContent = dedent`
     module "vpc" {
@@ -47,8 +46,8 @@ export default class Advanced {
       namespace   = var.app_name
       owner       = var.owner
       environment = var.environment
-    }`;
+    }`
 
-    appendToFile('main.tf', vpcModuleContent, this.options);
+    appendToFile('main.tf', vpcModuleContent, this.options)
   }
 }
