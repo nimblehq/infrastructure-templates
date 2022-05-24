36527c578bb71dec8e755e0c058686b64bf54474
Hoang Mirs <hoang.mirs@gmail.com>
2022-05-24 11:46:57 +0700

    Add descriptions

--- skeleton/aws/modules/vpc/variables.tf
+++ skeleton/aws/modules/vpc/variables.tf
@@ -1,4 +1,5 @@
 variable "namespace" {
+  description = "Namespace for the Terraform code"
 }
 
 variable "cidr" {
@@ -7,35 +8,45 @@ variable "cidr" {
 }
 
 variable "private_subnet_cidrs" {
+  description = "VPC private subnet CIDRs"
   type    = list(any)
   default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
 variable "public_subnet_cidrs" {
+  description = "VPC public subnet CIDRs"
   type    = list(any)
   default = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 }
 
 variable "enable_nat_gateway" {
+  description = "VPC NAT gateway flag"
   type    = bool
   default = true
 }
 
 variable "single_nat_gateway" {
+  description = "VPC single NAT gateway flag"
   type    = bool
   default = true
 }
 
 variable "one_nat_gateway_per_az" {
+  description = "VPC one NAT gateway per AZ flag"
   type    = bool
   default = false
 }
 
 variable "enable_dns_hostnames" {
+  description = "VPC DNS hostnames flag"
   type    = bool
   default = true
 }
 
-variable "environment" {}
+variable "environment" {
+  description = "The environment of the app, e.g. `acme-staging`"
+}
 
-variable "owner" {}
+variable "owner" {
+  description = "The owner of the infrastructure, e.g. `acme-web`"
+}
