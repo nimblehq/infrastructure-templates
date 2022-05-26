bc8df6ebe52d8ff7a68f34d0ec2fefc6eda44fb8
Junan Chakma <junan@nimblehq.co>
2022-05-26 16:22:08 +0700

    [19] Update RDS issue

--- src/templates/aws/advanced.ts
+++ src/templates/aws/advanced.ts
@@ -24,10 +24,10 @@ export default class Advanced {
     this.applyVpc();
     this.applySecurityGroup();
     this.applyEcr();
-    // this.applyLog();
+    this.applyLog();
     this.applyS3();
     this.applyAlb();
-    // this.applyRds();
+    this.applyRds();
     this.applyBastionInstance();
     this.applySsm();
     this.applySsm();
@@ -196,6 +196,22 @@ export default class Advanced {
     });
   }
 
+  private applyLog(): void {
+    copyDir("aws/modules/log", "modules/log", this.options);
+
+    const logModuleContent = dedent`
+    module "log" {
+      source = "../modules/log"
+
+      namespace = var.app_name
+    }
+    `;
+
+    injectToFile("main.tf", logModuleContent, this.options, {
+      insertAfter: "# Log",
+    });
+  }
+
   private applyAlb(): void {
     copyDir("aws/modules/alb", "modules/alb", this.options);
 
@@ -227,6 +243,67 @@ export default class Advanced {
 
     injectToFile("main.tf", albModuleContent, this.options, {
       insertAfter: "# ALB",
+    });
+  }
+
+  private applyRds(): void {
+    copyDir("aws/modules/rds", "modules/rds", this.options);
+
+    const rdsVariablesContent = dedent`
+    variable "rds_instance_type" {
+      description = "The RDB instance type"
+      type        = string
+    }
+
+    variable "rds_database_name" {
+      description = "RDS database name"
+      type = string
+    }
+
+    variable "rds_username" {
+      description = "RDS username"
+      type = string
+    }
+
+    variable "rds_password" {
+      description = "RDS password"
+      type = string
+    }
+
+    variable "rds_autoscaling_min_capacity" {
+      description = "Minimum number of RDS read replicas when autoscaling is enabled"
+      type = number
+    }
+
+    variable "rds_autoscaling_max_capacity" {
+      description = "Maximum number of RDS read replicas when autoscaling is enabled"
+      type =
+    }\n\n`;
+    appendToFile("variables.tf", rdsVariablesContent, this.options);
+
+    const albModuleContent = dedent`
+    module "rds" {
+      source = "../modules/rds"
+
+      namespace = var.app_name
+
+      vpc_security_group_ids = module.security_group.rds_security_group_ids
+      vpc_id                 = module.vpc.vpc_id
+
+      subnet_ids = module.vpc.private_subnet_ids
+
+      instance_type = var.rds_instance_type
+      database_name = var.rds_database_name
+      username      = var.rds_username
+      password      = var.rds_password
+
+      autoscaling_min_capacity = var.rds_autoscaling_min_capacity
+      autoscaling_max_capacity = var.rds_autoscaling_max_capacity
+    }
+    `;
+
+    injectToFile("main.tf", albModuleContent, this.options, {
+      insertAfter: "# RDS",
     });
   }
 
