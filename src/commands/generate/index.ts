30cbb9e9281fe2a25c4ee254043fd5352ba9fa19
Junan Chakma <junan@nimblehq.co>
2022-05-26 15:35:32 +0700

    [19] Use ecr

--- src/commands/generate/index.ts
+++ src/commands/generate/index.ts
@@ -1,6 +1,6 @@
-import {Command} from '@oclif/core'
-import * as inquirer from 'inquirer'
-import Advanced from '../../templates/aws/advanced'
+import { Command } from "@oclif/core";
+import * as inquirer from "inquirer";
+import Advanced from "../../templates/aws/advanced";
 
 type GenerateOption = {
   projectName: string;
@@ -9,90 +9,88 @@ type GenerateOption = {
 };
 
 export default class Hello extends Command {
-  static description = 'Generate infrastructure template command'
+  static description = "Generate infrastructure template command";
 
-  static examples = [
-    '$ nimble-infra generate',
-  ]
+  static examples = ["$ nimble-infra generate"];
 
   static flags = {};
 
   static args = [
     {
-      name: 'projectName',
+      name: "projectName",
       required: true,
-      description: 'directory name of new project',
-      default: '.',
+      description: "directory name of new project",
+      default: ".",
     },
   ];
 
   async run(): Promise<void> {
-    const {args} = await this.parse(Hello)
+    const { args } = await this.parse(Hello);
 
     const questions = [
       {
-        type: 'list',
-        name: 'platform',
-        message: 'Which cloud provider would you like to use?',
+        type: "list",
+        name: "platform",
+        message: "Which cloud provider would you like to use?",
         choices: [
           {
-            value: 'aws',
-            name: 'AWS',
+            value: "aws",
+            name: "AWS",
           },
           {
-            value: 'gcp',
-            name: 'GCP',
+            value: "gcp",
+            name: "GCP",
           },
           {
-            value: 'heroku',
-            name: 'Heroku',
+            value: "heroku",
+            name: "Heroku",
           },
         ],
       },
-    ]
+    ];
 
-    const platformChoice = await inquirer.prompt(questions)
+    const platformChoice = await inquirer.prompt(questions);
 
-    if (platformChoice.platform === 'aws') {
+    if (platformChoice.platform === "aws") {
       const questions = [
         {
-          type: 'list',
-          name: 'infrastructureType',
-          message: 'What kind of infrastructure do you need?',
+          type: "list",
+          name: "infrastructureType",
+          message: "What kind of infrastructure do you need?",
           choices: [
             {
-              key: 'basic',
-              value: 'basic',
-              name: 'Basic infrastructure (VPC + RDS + LOG + ECS)',
+              key: "basic",
+              value: "basic",
+              name: "Basic infrastructure (VPC + RDS + LOG + ECS)",
             },
             {
-              key: 'advanced',
-              value: 'advanced',
-              name: 'Complete infrastructure (VPC + RDS + S3 + FARGATE + LOG + Security groups + ALB)',
+              key: "advanced",
+              value: "advanced",
+              name: "Complete infrastructure (VPC + ECR + RDS + S3 + FARGATE + LOG + Security groups + ALB)",
             },
           ],
         },
-      ]
+      ];
 
-      const infrastructureType = await inquirer.prompt(questions)
+      const infrastructureType = await inquirer.prompt(questions);
 
       const options: GenerateOption = {
         projectName: args.projectName,
         platform: platformChoice.platform,
         infrastructureType: infrastructureType.infrastructureType,
-      }
+      };
 
       switch (options.infrastructureType) {
-      case 'advanced':
-        Advanced.run(options)
-        break
-      case 'basic':
-      default:
-        console.log('This type has not been implemented!')
-        break
+        case "advanced":
+          Advanced.run(options);
+          break;
+        case "basic":
+        default:
+          console.log("This type has not been implemented!");
+          break;
       }
     }
   }
 }
 
-export type {GenerateOption}
+export type { GenerateOption };
