1dbb0f3544629474ae09fb187cb9deff75396c15
Hoang Mirs <hoang.mirs@gmail.com>
2022-07-18 16:49:28 +0700

    [#66] Migrate tests from using mocha to jest

--- test/commands/generate/index.test.ts
+++ test/commands/generate/index.test.ts
@@ -1,19 +1,24 @@
-import { test } from '@oclif/test';
+import { test, expect } from '@oclif/test';
+import * as fs from 'fs-extra';
 import * as inquirer from 'inquirer';
-import * as sinon from 'sinon';
 
 describe('running command generate', () => {
-  const stubResponse = sinon.stub()
-    .onFirstCall()
-    .resolves({ platform: 'aws' })
-    .onSecondCall()
-    .resolves({ infrastructureType: 'advance' });
+  const projectName = 'app-name';
+
+  afterEach(() => {
+    jest.resetAllMocks();
+    fs.removeSync(projectName);
+  });
+
+  const stubResponse = jest.fn();
+  stubResponse.mockReturnValueOnce({ platform: 'aws' });
+  stubResponse.mockReturnValueOnce({ infrastructureType: 'advanced' });
 
   test
     .stdout()
     .stub(inquirer, 'prompt', stubResponse)
-    .command(['generate'])
-    .it('lists cloud provider', async(_, done) => {
-      done();
+    .command(['generate', projectName])
+    .it('creates a new project folder', () => {
+      expect(fs.existsSync(projectName)).to.eq(true);
     });
 });
