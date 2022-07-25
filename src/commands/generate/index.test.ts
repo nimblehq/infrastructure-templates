6898b42b3ed62b627667c18b62d8546260f885fe
Hoang Mirs <hoang.mirs@gmail.com>
2022-07-25 18:07:25 +0700

    [#29] Copy steps instead of putting on beforeEach

--- src/commands/generate/index.test.ts
+++ src/commands/generate/index.test.ts
@@ -10,24 +10,29 @@ describe('running command generate directly', () => {
   const projectDir = 'app-name';
   const stdoutSpy = jest.spyOn(process.stdout, 'write');
 
-  beforeEach(async() => {
+  beforeEach(() => {
     (prompt as unknown as jest.Mock)
       .mockResolvedValueOnce({ provider: 'aws' })
       .mockResolvedValueOnce({ infrastructureType: 'advanced' });
-
-    await Generator.run([projectDir]);
   });
 
   afterEach(() => {
     jest.resetAllMocks();
+  });
+
+  it('creates a new project folder', async() => {
+    await Generator.run([projectDir]);
+
+    expect(existsSync(projectDir)).toBe(true);
+
     remove('/', projectDir);
   });
 
-  it('creates a new project folder', () => {
-    expect(existsSync(projectDir)).toBe(true);
-  });
+  it('displays the success message', async() => {
+    await Generator.run([projectDir]);
 
-  it('displays the success message', () => {
     expect(stdoutSpy).toHaveBeenCalledWith('The infrastructure has been generated!\n');
+
+    remove('/', projectDir);
   });
 });
